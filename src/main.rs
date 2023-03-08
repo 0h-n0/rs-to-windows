ab3b4d9455c263e6de892a7b77d16d3cdb9624d7
0h-n0 <kbu94982@gmail.com>
2023-03-09 03:34:59 +0900

    feat: update

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,16 @@
 use actix_web::{web, App, HttpResponse, HttpServer};
 
 fn main() {
-    let server = HttpServer::new(|| {
-        App::new()
-        .route("/", web::get().to(get_index))
-    });
+    let server = HttpServer::new(|| App::new().route("/", web::get().to(get_index)));
     println!("Serving on http://localhost:3000...");
-    server.bind("127.0.0.1:3000").expect("error binding server to address").run().expect("error running server");    
+    server
+        .bind("127.0.0.1:3000")
+        .expect("error binding server to address")
+        .run()
+        .expect("error running server");
 }
 fn get_index() -> HttpResponse {
-    HttpResponse::Ok()
-    .content_type("text/html")
-    .body(
+    HttpResponse::Ok().content_type("text/html").body(
         r#"
         <title>GCD Calculator</title>
         <form action="/gcd" method="post">
@@ -19,6 +18,6 @@ fn get_index() -> HttpResponse {
         <input type="text" name="m"/>
         <button type="submit>Compute GCD</button>
         </form>
-        "#
+        "#,
     )
 }
